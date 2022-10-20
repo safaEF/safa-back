f8bb9159b1f3ceb9ba87b5ae0ce6b47f6dc699f1
tunichess <trabelsisofiane93@gmail.com>
2022-10-20 13:34:54 +0200

    .env

--- testproject/settings.py
+++ testproject/settings.py
@@ -22,9 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY", default="unsafe-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True   
-
-
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 if not DEBUG:
@@ -85,7 +83,6 @@ WSGI_APPLICATION = 'testproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-print(os.environ.get('db_name','') ,"***************")
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
